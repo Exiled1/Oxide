da5b78a8ee046f3e9824c2f7ca7335cf05761d9c
Rudy Peralta <rudyperalta831@gmail.com>
2021-08-30 21:17:39 -0700

    Fixed dependency errors, got stuff to print, VGA wrapper WIP.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,27 @@
 #![no_std]
 #![no_main]
+
 use core::panic::PanicInfo;
- 
+
+static HELLO: &[u8] = b"Hello World!";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    loop{}
+    // VGA buffer starts at 0xb8000, 0xA0000 is for something else.
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
+    loop {}
 }
 
+/// This function is called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    loop {
-        
-    }
+    loop {}
 }
